0c856119c36b9c40f50a64265d6e062b5f18ffeb
Nurlybek Sultamuratov <nsultamu@my.centennialcollege.ca>
2020-07-27 10:48:59 -0500

    Added second form

--- COMP123-Summer2020-Lesson10/MainForm.Designer.cs
+++ COMP123-Summer2020-Lesson10/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace COMP123_Summer2020_Lesson10
 {
-    partial class StartForm
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,48 +29,56 @@
         private void InitializeComponent()
         {
             this.NextButton = new System.Windows.Forms.Button();
-            this.StartFormLabel = new System.Windows.Forms.Label();
+            this.MainFormLabel = new System.Windows.Forms.Label();
+            this.BackButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // NextButton
             // 
-            this.NextButton.Location = new System.Drawing.Point(462, 388);
+            this.NextButton.Location = new System.Drawing.Point(462, 390);
             this.NextButton.Name = "NextButton";
             this.NextButton.Size = new System.Drawing.Size(150, 50);
-            this.NextButton.TabIndex = 0;
+            this.NextButton.TabIndex = 2;
             this.NextButton.Text = "Next";
             this.NextButton.UseVisualStyleBackColor = true;
             // 
-            // StartFormLabel
+            // MainFormLabel
             // 
-            this.StartFormLabel.AutoSize = true;
-            this.StartFormLabel.Location = new System.Drawing.Point(265, 159);
-            this.StartFormLabel.Name = "StartFormLabel";
-            this.StartFormLabel.Size = new System.Drawing.Size(55, 13);
-            this.StartFormLabel.TabIndex = 1;
-            this.StartFormLabel.Text = "Start Form";
-            this.StartFormLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.MainFormLabel.AutoSize = true;
+            this.MainFormLabel.Location = new System.Drawing.Point(282, 181);
+            this.MainFormLabel.Name = "MainFormLabel";
+            this.MainFormLabel.Size = new System.Drawing.Size(56, 13);
+            this.MainFormLabel.TabIndex = 3;
+            this.MainFormLabel.Text = "Main Form";
+            this.MainFormLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // StartForm
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(3, 390);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(150, 50);
+            this.BackButton.TabIndex = 2;
+            this.BackButton.Text = "Back";
+            this.BackButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 441);
-            this.Controls.Add(this.StartFormLabel);
+            this.Controls.Add(this.MainFormLabel);
+            this.Controls.Add(this.BackButton);
             this.Controls.Add(this.NextButton);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
-            this.Name = "StartForm";
-            this.Text = "StartForm";
+            this.Name = "MainForm";
+            this.Text = "MainForm";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
         private System.Windows.Forms.Button NextButton;
-        private System.Windows.Forms.Label StartFormLabel;
+        private System.Windows.Forms.Label MainFormLabel;
+        private System.Windows.Forms.Button BackButton;
     }
 }
-
