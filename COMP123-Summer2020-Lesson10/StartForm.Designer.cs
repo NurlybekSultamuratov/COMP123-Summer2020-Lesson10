fcfa487a5ef76a5af12c92c964612914159788bb
Nurlybek Sultamuratov <nsultamu@my.centennialcollege.ca>
2020-07-27 10:42:46 -0500

    Add project files.

---
+++ COMP123-Summer2020-Lesson10/StartForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace COMP123_Summer2020_Lesson10
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NextButton = new System.Windows.Forms.Button();
+            this.StartFormLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(462, 388);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(150, 50);
+            this.NextButton.TabIndex = 0;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            // 
+            // StartFormLabel
+            // 
+            this.StartFormLabel.AutoSize = true;
+            this.StartFormLabel.Location = new System.Drawing.Point(231, 151);
+            this.StartFormLabel.Name = "StartFormLabel";
+            this.StartFormLabel.Size = new System.Drawing.Size(55, 13);
+            this.StartFormLabel.TabIndex = 1;
+            this.StartFormLabel.Text = "Start Form";
+            this.StartFormLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.StartFormLabel);
+            this.Controls.Add(this.NextButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "StartForm";
+            this.Text = "StartForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NextButton;
+        private System.Windows.Forms.Label StartFormLabel;
+    }
+}
+
